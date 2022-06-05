01713e8f3670bcf6119d0c815287b66289015221
Moshe Sherman <mtshrmn@gmail.com>
2022-06-06 02:08:50 +0300

    implement hw4q1

--- src/hw4q1.c
+++ src/hw4q1.c
@@ -3,11 +3,10 @@
 #define I_KNOW 1
 #define I_DONT_KNOW 0
 #define N  500
-
-
-// ADD YOUR DEFINES HERE
-
-
+#define CHAR_MAX 256
+// make sure modulo returns non negative value.
+#define MODULO(A, B) ((A % B + B) % B)
+#define MAX(A, B) (A > B ? A : B)
 
 //Include files
 #include <stdio.h>
@@ -15,13 +14,10 @@
 #include <stdbool.h>
 #include <string.h>
 
-
-//Function declarations ..
+// function declerations
 void PrintIDontKnow();
 void PrintIKnow();
 void test();
-
-//Function declarations to be implemented by you :)
 bool isPalindrome(char* str);
 bool isMixedPalindrome(char* str);
 bool isKCyclicPalindrome(char* str, int k);
@@ -35,7 +31,6 @@ int main()
 	test();
 	return 0;
 }
-
 void test()
 {
 	char str[N]={0};
@@ -78,49 +73,112 @@ void test()
     printf("Longest mixed substring is:%d\n", longest);
 
 }
-
-
 void PrintIDontKnow()
 {
 	printf("%d\n", I_DONT_KNOW);
 }
-
 void PrintIKnow()
 {
 	printf("%d\n", I_KNOW);
 }
 
+bool isPalindrome(char* str) {
+	int left = 0;
+	int right = strlen(str) - 1;
 
-
-//Your code goes here .. ENJOY!!
-
-bool isPalindrome(char* str)
-{
-
-   //TODO..ADD your code here. Feel free to change the return statement bellow.
-    return true;
+	while (right > left) {
+		if (str[left] != str[right]) {
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
 }
 
-
-
-bool isMixedPalindrome(char* str)
-{
-
-    //TODO..ADD your code here. Feel free to change the return statement bellow.
-    return true;
-
+bool isMixedPalindrome(char* str) {
+	int char_num[CHAR_MAX] = {0};
+	int odd_count = 0;
+	// same as iterating until i < strlen(str).
+	// directly checking != '\0' is faster.
+	for (int i = 0; str[i] != '\0'; ++i) {
+		char_num[(int) str[i]]++;
+	}
+	// check the amount of odd occurrences
+	// if there's more than 1, it's not a mixed palindrome.
+	for (int i = 0; i < CHAR_MAX; ++i) {
+		if (char_num[i] % 2 == 1) {
+			if (odd_count == 1) {
+				return false;
+			}
+			odd_count++;
+		}
+	}
+	return true;
 }
 
+bool isKCyclicPalindrome(char* str, int k) {
+	int len = strlen(str);
+	// rotating the string `len` times is equivalent to not rotating at all.
+	// that way we make sure that len is always bigger than k.
+	int modulo_k = k % len;
+	int left = modulo_k;
+	int right = MODULO((modulo_k - 1), len);
 
-bool isKCyclicPalindrome(char* str, int k)
-{
-   //TODO..ADD your code here. Feel free to change the return statement bellow.
-    return true;
+	for (int i = 1; i < len / 2; ++i) {
+		if (str[left] != str[right]) {
+			return false;
+		}
+		left = MODULO((left + 1), len);
+		right = MODULO((right - 1), len);
+	}
+	return true;
 }
 
+int getLongestMixedSubstring(char* str) {
+	int len = strlen(str);
+	int count[256] = {0};
+	int odd_count = 0;
+	int max = 0;
 
-int getLongestMixedSubstring(char* str)
-{
-    //TODO..ADD your code here. Feel free to change the return statement bellow.
-    return 0;
+	// we start from the beginning of the string, and go forwards.
+	// inside the loop we will go backwards until we find a mixed-palindrome.
+	for (int start = 0; start < len; ++start) {
+		// reset all variables.
+		odd_count = 0;
+		for (int i = 0; i < 256; ++i) {
+			count[i] = 0;
+		}
+
+		// count all letters in str. I CANNOT BELIEVE IT'S HISTOGRAM.
+		for (int end = len - 1; end >= start; --end) {
+			count[(int) str[end]]++;
+		}
+
+		// calculate how many odd occurrences there are from `start` to real end.
+		for (int i = 0; i < 256; ++i) {
+			if (count[i] % 2 == 1) {
+				odd_count++;
+			}
+		}
+
+		// each time, go back from the end and check if we have less than 2 odds.
+		// if yes, it's a mixed palindrome, update the max palindrome if needed.
+		// if no, update the occurrences and subtract from the odds if it's an odd.
+		for (int end = len - 1; end >= start; --end) {
+			if (odd_count <= 1) {
+				max = MAX(max, (end - start + 1));
+				// because we're moving backwards, the first time we get to this line-
+				// we are guaranteed to get the biggest mixed-palindrome for the current `start`
+				// so we can safely break and iterate over the next starting point.
+				break;
+			}
+			if (count[(int) str[end]] % 2 == 0 && count[(int) str[end]] > 1) {
+				odd_count--;
+			}
+			count[(int)str[end]]--;
+		}
+	}
+  return max;
 }
+
