3bc8c717ff17ffc6e4e4aa0e491afcb9388aa7fe
Moshe Sherman <mtshrmn@gmail.com>
2022-06-26 22:52:12 +0300

    implement hw5q1

--- src/hw5q1.c
+++ src/hw5q1.c
@@ -14,6 +14,8 @@
 #define N 4
 #define NO_ROAD -1
 #define NO_PATH -1
+#define INT_MAX 2147483647
+#define MIN(A, B) (A > B ? B : A)
 
 int ShortestFullPath(int roads[N][N]);
 
@@ -26,13 +28,49 @@ void PrintLenOfShortestFullPath(int min_len);
  -------------------------------------------------------------------------*/
 int main()
 {
-    // implement
+    PrintRoadsInputMessage();
+    int roads[N][N];
+    for (int row = 0; row < N; ++row) {
+        for (int col = 0; col < N; ++col) {
+            scanf("%d", &roads[row][col]);
+        }
+    }
+    PrintLenOfShortestFullPath(ShortestFullPath(roads));
     return 0;
+}
+
+void tsp(roads, visited, current_city, visited_city_count, cost_till_now, min_distance)
+    int roads[N][N];
+    bool visited[N];
+    int current_city;
+    int visited_city_count;
+    int cost_till_now;
+    int *min_distance;
+{
+    if (visited_city_count == N - 1 && roads[current_city][0] > 0) {
+        *min_distance =  MIN(*min_distance, (cost_till_now + roads[current_city][0]));
+    }
+
+    for (int next_city = 1; next_city < N; ++next_city) {
+        int next_distance = roads[current_city][next_city];
+        if (visited[next_city] == false && next_distance > 0) {
+            visited[next_city] = true;
+            tsp(roads, visited, next_city, visited_city_count + 1,
+                cost_till_now + next_distance, min_distance);
+            visited[next_city] = false;
+        }
+    }
 }
 
 // visit all cities and look for shortest path based on roads
 int ShortestFullPath(int roads[N][N]) {
-    return 0;
+    bool visited[N] = {false};
+    int min_distance = INT_MAX;
+    tsp(roads, visited, 0, 0, 0, &min_distance);
+    if (min_distance == INT_MAX) {
+        return NO_PATH;
+    }
+    return min_distance;
 }
 
 // Print functions
