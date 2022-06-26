58ace0bd99ec45297641d8aba148ddd08379122e
Moshe Sherman <mtshrmn@gmail.com>
2022-06-26 22:53:05 +0300

    partial impelement of hw5q2 - debug needed

--- src/hw5q2.c
+++ src/hw5q2.c
@@ -3,6 +3,7 @@
 --------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 /*=========================================================================
   Constants and definitions:
@@ -12,6 +13,7 @@
 // declarations
 bool isPalindrome(unsigned int x);
 int represent(unsigned int x);
+void sort_arr(unsigned int *arr, int len);
 
 // Print functions declarations
 void printArr(unsigned int arr[], int size);
@@ -20,16 +22,73 @@ void printArr(unsigned int arr[], int size);
   The main program. (describe what your program does here)
  -------------------------------------------------------------------------*/
 int main() {
-    // implement
+    int len;
+    scanf("%d", &len);
+
+    unsigned int *arr = malloc(sizeof(unsigned int) * len);
+    if (arr == NULL) {
+        return 0;
+    }
+    for (int i = 0; i < len; ++i) {
+        scanf("%u", arr + i);
+    }
+    sort_arr(arr, len);
+    printArr(arr, len);
     return 0;
+}
+
+bool reverse_int(unsigned int x, unsigned int partial) {
+    if (x == 0) {
+        return partial;
+    }
+    return reverse_int(x / 10, partial * 10 + x % 10);
 }
 
 bool isPalindrome(unsigned int x){
-    return false;
+    return reverse_int(x, 0) == x;
 }
 
+
 int represent(unsigned int x){
-    return 0;
+    if (x <= 9) {
+        return x;
+    }
+    unsigned int product = 1;
+    while (x > 0) {
+        unsigned int remainder = x % 10;
+        product *= remainder;
+        x /= 10;
+    }
+    return represent(product);
+}
+
+int order(unsigned int a, unsigned int b) {
+    int is_a_pal = isPalindrome(a);
+    int is_b_pal = isPalindrome(b);
+    if (is_a_pal != is_b_pal) {
+        return is_b_pal - is_a_pal;
+    }
+
+    int represent_a = represent(a);
+    int represent_b = represent(b);
+    if (represent_b != represent_a) {
+        return represent_a - represent_b;
+    }
+
+    return a - b;
+}
+
+void sort_arr(unsigned *arr, int len) {
+    for (int i = 1; i < len; ++i) {
+        for (int j = 1; j < len; ++j) {
+            if (order(arr[i], arr[j]) < 0) {
+                // swap items
+                unsigned int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
 }
 
 // print function
